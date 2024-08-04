1d66882977f6c09b603e4b4ec7e16203aef1e443
wraleydi <raleydi06@gmail.com>
2024-08-05 01:38:35 +0700

    penambahan styling

--- src/script/components/header-app.js
+++ src/script/components/header-app.js
@@ -15,8 +15,8 @@ class HeaderApp extends HTMLElement {
                 font-size: 24px;
             }
             .profile-img img {
-                width: 50px;
-                height: 50px;
+                width: 40px;
+                height: 40px;
                 border-radius: 50%;
             }
         `;
