b50a332c3ee15938d0c5e971b8c190b58080cf50
wraleydi <raleydi06@gmail.com>
2024-08-04 09:49:29 +0700

    add note-item.js

---
+++ src/script/components/note-item.js
@@ -0,0 +1,53 @@
+class NoteItem extends HTMLElement {
+    static get observedAttributes() {
+        return ["id", "title", "body", "createdAt", "archived"];
+    }
+
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+        this._id = '';
+        this._title = '';
+        this._body = '';
+        this._createdAt = '';
+        this._archived = false;
+    }
+
+    connectedCallback() {
+        this.render();
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (oldValue !== newValue) {
+            this[`_${name}`] = newValue;
+            this.render();
+        }
+    }
+
+    render() {
+        this.shadowRoot.innerHTML = `
+            <style>
+                .notes {
+                    border: 1px solid #ccc;
+                    padding: 10px;
+                    margin: 10px 0;
+                    border-radius: 5px;
+                }
+                .title h2 {
+                    margin: 0;
+                }
+            </style>
+            <div class="notes">
+                <div class="title">
+                    <h2>${this._title}</h2>
+                </div>
+                <div class="body">
+                    <p>${this._body}</p>
+                </div>
+                <small>${new Date(this._createdAt).toLocaleString()}</small>
+            </div>
+        `;
+    }
+}
+
+customElements.define("note-item", NoteItem);
